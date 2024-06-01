1544675d36a75f79d0496ba7e5207f50b88ecd76
Gabriela Biswasova <pracho.dayat@seznam.cz>
2024-06-01 18:21:44 +0200

    Faze 2

--- src/components/Wordbox/index.tsx
+++ src/components/Wordbox/index.tsx
@@ -1,29 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
 
-interface IWordboxProp {
+interface IWordboxProps {
   word: string;
+  onFinish: () => void;
 }
 
-const Wordbox: React.FC<IWordboxProp> = ({ word }) => {
+const Wordbox: React.FC<IWordboxProps> = ({ word, onFinish }) => {
   const [lettersLeft, setLettersLeft] = useState<string>(word);
-  useEffect(
-    () => {
-      const handleKeyUp = (event: KeyboardEvent) => {
-        if(lettersLeft.length >0){
+
+  useEffect(() => {
+    const handleKeyUp = (event: KeyboardEvent) => {
+      if (lettersLeft.length > 0) {
         const firstLetter = lettersLeft.charAt(0);
         if (event.key === firstLetter) {
           setLettersLeft(lettersLeft.slice(1));
-        }}
-        console.log('pressed',event.key)
-      };
-      window.addEventListener('keyup', handleKeyUp);
-      return () => {
-        window.removeEventListener('keyup', handleKeyUp);
-      };
-    },
-    [lettersLeft],
-  );
+          if (lettersLeft.length === 1) {
+            onFinish();
+          }
+        }
+      }
+    };
+    window.addEventListener('keyup', handleKeyUp);
+    return () => {
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, [lettersLeft, onFinish]);
 
   return <div className="wordbox">{lettersLeft}</div>;
 };
